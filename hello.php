4262af44240d6fcff3830970c9ad724333abbb03
unknown <muskaanqadir9@gmail.com>
2026-09-13 21:11:29 +0530

    Add hello.php

---
+++ hello.php
@@ -0,0 +1,9 @@
+<?php
+$a = 10;
+$b = 5;
+echo "addition:" . ($a + $b) . "<br>";
+echo "subtraction:" . ($a - $b) . "<br>";
+echo "multiplication:" . ($a * $b) . "<br>";
+echo "division:" . ($a / $b) . "<br>";
+echo "modulus:" . ($a % $b) . "<br>";
+?>
